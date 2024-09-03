17d7e054ecf1cc046173729538a3939308d09ba4
Igor S. Morozov <igor@morozov.is>
2024-09-03 22:44:37 +0200

    Do not use strict-dynamic in style-src as it does not exist

--- app/controllers/attacks/strict_dynamic_controller.rb
+++ app/controllers/attacks/strict_dynamic_controller.rb
@@ -4,7 +4,7 @@ module Attacks
       policy = request.content_security_policy.dup
 
       policy.script_src :strict_dynamic
-      policy.style_src :strict_dynamic
+      policy.style_src :self
 
       request.content_security_policy = policy
     end
