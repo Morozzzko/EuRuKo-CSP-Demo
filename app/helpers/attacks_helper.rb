00d4591e322afe28ac18ce8ce229aaafecd57eb1
Igor S. Morozov <igor@morozov.is>
2024-09-03 22:47:26 +0200

    Display actually calculated CSP nonce

--- app/helpers/attacks_helper.rb
+++ app/helpers/attacks_helper.rb
@@ -1,6 +1,12 @@
 module AttacksHelper
   def csp_as_hash
-    request.content_security_policy.build.split(";").each_with_object({}) do |directive, hash|
+    nonce = request.content_security_policy_nonce
+    nonce_directives = request.content_security_policy_nonce_directives
+    context = request.controller_instance || request
+
+    request.content_security_policy.build(
+      context, nonce, nonce_directives
+    ).split(";").each_with_object({}) do |directive, hash|
       name, *rest = directive.split(" ")
 
       hash[name] = rest.join(" ")
